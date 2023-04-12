ebb239321db0c79c53b582acaee5715d316b6111
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-12 15:58:29 +0530

    Add with size

--- Program.cs
+++ Program.cs
@@ -8,7 +8,18 @@
             BinarySearchTree<int> obj = new BinarySearchTree<int>(56);
             obj.Add(30);
             obj.Add(70);
+            obj.Add(22);
+            obj.Add(40);
+            obj.Add(60);
+            obj.Add(95);
+            obj.Add(11);
+            obj.Add(65);
+            obj.Add(3);
+            obj.Add(16);
+            obj.Add(63);
+            obj.Add(67);
             obj.Display();
+            obj.Size();
         }
     }
 
