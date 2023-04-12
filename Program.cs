90b7275c8011b07829c1a1746d4cc1f25ba6853a
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-12 15:41:49 +0530

    Create Tree

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree and Hash Table Program");
+            BinarySearchTree<int> obj = new BinarySearchTree<int>(56);
+            obj.Add(30);
+            obj.Add(70);
+            obj.Display();
         }
     }
 
